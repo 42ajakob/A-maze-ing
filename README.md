c2b2bd9e39078384f6c701e26728bcca53cfb3e2
Zoe Maurer <zmaurer@2-B-5.42heilbronn.de>
2026-09-08 21:16:54 +0200

    Maze generation - return open neighbours. README

--- README.md
+++ README.md
@@ -1,13 +1,131 @@
-This project has been created as part
-of the 42 curriculum by anjakob, zmaurer.
+*This project has been created as part of the 42 curriculum by anjakob, zmaurer.*
 
-**Description**
+# A-Maze-ing
 
-**Instructions**
 
-**Resources**
+## Description
 
-**Addition**
+A-Maze-ing is a Python project developed as part of the 42 curriculum.
+
+The objective of the project is to generate a valid random maze from a configuration file, write it to an output file using the required hexadecimal wall representation, display it visually to the user, and to prodive a shortest path between entrance and exit.
+
+The project explores concepts like:
+* Maze generation algorithms
+* Pathfinding
+* Random generation and reproducibility through seeds
+* File parsing and validation
+* Data structures
+* Terminal-based visualisation
+* Object-oriented programming
+* Python type hints and static analysis
+* Code reusability and package creation
+
+The generated maze must respect the constraints defined by the subject, including valid borders, coherent walls between neighbouring cells, connectivity, corridor-width limitations, and the optional perfect-maze requirement.
+
+When possible, the maze also contains a visible 42 pattern made from closed cells.
+
+
+## Instructions
+
+### Requirements
+
+* Python **3.10 or later**
+* `pip`
+* `flake8`
+* `mypy`
+
+### Installation
+
+Install the dependencies with:
+
+```bash
+make install
+```
+
+### Running the project
+
+To execute the main program, the subject's execution format requires:
+
+```bash
+python3 a_maze_ing.py config.txt
+```
+
+The configuration filename can be changed, if it is passed as the only argument.
+
+The project includes a `Makefile` for common development tasks:
+
+```bash
+make install
+make run
+make clean
+```
+
+To run the program using Python's built-in debugger:
+
+```bash
+make debug
+```
+
+To run both `flake8` and `mypy` using the required checks:
+
+```bash
+make lint
+```
+
+
+## Configuration file
+
+
+## Maze Generation Algorithm
+
+### Depth-First Search (DFS)
+
+
+## Perfect and imperfect mazes
+
+### Perfect mazes
+
+### Imperfect mazes
+
+
+## 42 Pattern
+
+
+## Maze Representation
+
+
+## Output File Format
+
+## Breadth-First Search (BFS)
+
+## Visual Representation
+
+
+## Code Reusability
+
+
+## Project Management and Roles
+
+The project was developed by **anjakob** and **zmaurer**. The tasks were distributed evenly:
+
+### anjakob
+Responsible for the parsing and visual representation:
+
+
+### zmaurer
+Responsible for the maze generation and ...:
+
+
+## Planning and Evolution
+
+At the start of the project, the tasks were divived into 4 parts: parsing the config file, maze generation, writing the maze into an output file and visual representation (TUI).
+
+### What worked well
+
+### What could be improved
+
+
+## Resources
 
 • The complete structure and format of your config file.
 • The maze generation algorithm you chose.
--- utils/maze_gen.py
+++ utils/maze_gen.py
@@ -1,8 +1,15 @@
+import random
+
+NORTH = 0b0001
+EAST = 0b0010
+SOUTH = 0b0100
+WEST = 0b1000
+
 class Cell:
     """Single cell in the maze"""
 
     def __init__(self) -> None:
-        """Initialize cell with all walls closed"""
+        """Initialize a cell with all walls closed"""
         self.walls: int = 0b1111
 
 class MazeGenerator:
@@ -17,13 +24,14 @@ class MazeGenerator:
         perfect: bool,
         seed: int | None = None,
     ) -> None:
-        """Initialize maze generator"""
+        """Initialize the maze generator"""
         self.width: int = width
         self.height: int = height
         self.entry: tuple[int, int] = entry
         self.exit: tuple[int, int] = exit
         self.perfect: bool = perfect
         self.seed: int | None = seed
+        self.random = random.Random(seed)
 
         self.maze: list[list[Cell]] = [
             [Cell() for _ in range(width)]
@@ -31,51 +39,124 @@ class MazeGenerator:
         ]
 
     def remove_wall(
-        self,
-        first: tuple[int, int],
-        second: tuple[int, int],
+    	self,
+    	first: tuple[int, int],
+    	second: tuple[int, int],
     ) -> None:
-        """Open wall between two neighboring cells"""
+        """Open the wall between two neighbouring cells"""
         x1, y1 = first
         x2, y2 = second
+
+        if not (0 <= x1 < self.width and 0 <= y1 < self.height):
+            raise ValueError("First cell is outside of the maze")
+
+        if not (0 <= x2 < self.width and 0 <= y2 < self.height):
+            raise ValueError("Second cell is outside of the maze")
 
         cell1 = self.maze[y1][x1]
         cell2 = self.maze[y2][x2]
 
         if x2 == x1 + 1:
-            cell1.walls &= ~0b0010
-            cell2.walls &= ~0b1000
+            cell1.walls &= ~EAST
+            cell2.walls &= ~WEST
 
         elif x2 == x1 - 1:
-            cell1.walls &= ~0b1000
-            cell2.walls &= ~0b0010
+            cell1.walls &= ~WEST
+            cell2.walls &= ~EAST
 
         elif y2 == y1 + 1:
-            cell1.walls &= ~0b0100
-            cell2.walls &= ~0b0001
+            cell1.walls &= ~SOUTH
+            cell2.walls &= ~NORTH
 
         elif y2 == y1 - 1:
-            cell1.walls &= ~0b0001
-            cell2.walls &= ~0b0100
+            cell1.walls &= ~NORTH
+            cell2.walls &= ~SOUTH
 
         else:
-            raise ValueError("Cells are not neighboring cells.")
+            raise ValueError("Cells are not neighbouring cells")
 
+    def get_neighbours(
+    self,
+    position: tuple[int, int],
+    ) -> list[tuple[int, int]]:
+        """Return the neighboring cells. Which cells are connected?"""
+        x, y = position
 
-if __name__ == "__main__":
-    generator = MazeGenerator(
-        2,
-        1,
-        (0, 0),
-        (1, 0),
-        True,
-        42,
-    )
+        neighbours = []
 
-    generator.remove_wall((0, 0), (1, 0))
+        if y > 0:
+            neighbours.append((x, y - 1))
 
-    print("Left:", bin(generator.maze[0][0].walls))
-    print("Right:", bin(generator.maze[0][1].walls))
+        if x < self.width - 1:
+            neighbours.append((x + 1, y))
+
+        if y < self.height - 1:
+            neighbours.append((x, y + 1))
+
+        if x > 0:
+            neighbours.append((x - 1, y))
+
+        return neighbours
+
+    def get_open_neighbours(
+        self,
+        position: tuple[int, int],
+    ) -> list[tuple[int, int]]:
+        """Return the neighbouring cells connected by an open wall. Which cells can be walked to?"""
+        x, y = position
+        cell = self.maze[y][x]
+
+        open_neighbours = []
+
+        for neighbour in self.get_neighbours(position):
+            nx, ny = neighbour
+
+            """In which direction lays the neighbour?"""
+            if nx == x + 1:
+                direction = EAST
+            elif nx == x - 1:
+                direction = WEST
+            elif ny == y + 1:
+                direction = SOUTH
+            else:
+                direction = NORTH
+
+            """Check if the wall is open"""
+            if not (cell.walls & direction):
+                open_neighbours.append(neighbour)
+
+        return open_neighbours
+    
+    def generate(self) -> None:
+        """Generate a random maze"""
+        visited: set[tuple[int, int]] = set()
+        stack: list[tuple[int, int]] = []
+
+        current = (0, 0)
+        visited.add(current)
+        stack.append(current)
+
+        while stack:
+            current = stack[-1]
+            
+            """Find the unvisited neighbours"""
+            neighbors = self.get_neighbours(current)
+            unvisited = [
+                neighbor
+                for neighbor in neighbors
+                if neighbor not in visited
+            ]
+            """Pick one randomly and remove the wall"""
+            if unvisited:
+                next_cell = self.random.choice(unvisited)
+
+                self.remove_wall(current, next_cell)
+
+                visited.add(next_cell)
+                stack.append(next_cell)
+
+            else:
+                stack.pop()
 
 """
 TESTING FOLLOWS:
@@ -102,8 +183,8 @@ if __name__ == "__main__":
         for cell in row:
             print(bin(cell.walls), end=" ")
         print()
-"""
-
+        
+        
 if __name__ == "__main__":
     generator = MazeGenerator(
         2,
@@ -118,3 +199,52 @@ if __name__ == "__main__":
 
     print(bin(generator.maze[0][0].walls))
     print(bin(generator.maze[0][1].walls))
+
+    
+if __name__ == "__main__":
+    generator = MazeGenerator(
+        3,
+        3,
+        (0, 0),
+        (2, 2),
+        True,
+        42,
+    )
+
+    print("Center:", generator.get_neighbors((1, 1)))
+    print("Top-left:", generator.get_neighbors((0, 0)))
+    print("Bottom-right:", generator.get_neighbors((2, 2)))
+
+if __name__ == "__main__":
+    generator = MazeGenerator(
+        3,
+        3,
+        (0, 0),
+        (2, 2),
+        True,
+        42,
+    )
+
+    generator.generate()
+
+    for row in generator.maze:
+        for cell in row:
+            print(bin(cell.walls), end=" ")
+        print()
+"""
+
+if __name__ == "__main__":
+    generator = MazeGenerator(
+        2,
+        1,
+        (0, 0),
+        (1, 0),
+        True,
+        42,
+    )
+
+    print("Before:", generator.get_open_neighbours((0, 0)))
+
+    generator.remove_wall((0, 0), (1, 0))
+
+    print("After:", generator.get_open_neighbours((0, 0)))
