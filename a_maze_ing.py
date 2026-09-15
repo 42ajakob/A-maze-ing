a711e37a2e5ab2aad743adc48a69cdbfcb6353a4
anjakob <anjakob@student.42heilbronn.de>
2026-09-15 14:49:09 +0200

    filled template with values

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -5,9 +5,19 @@ def main() -> int:
     """DocStrings"""
     config = parser()
     try:
-        maze = MazeGenerator(config)
-        output_file = write_maze(maze)
-        tui(output_file)
+        maze = MazeGenerator(
+            width=config.width,
+            height=config.height,
+            entry=config.maze_entry,
+            exit=config.maze_exit,
+            perfect=config.perfect,
+            seed=config.seed,
+        )
+        maze.generate()
+        write_maze(
+            config.output_file, maze
+        )
+        tui(config.output_file)
     except Exception as e:
         print(f"Error: {e}")
         exit(1)
