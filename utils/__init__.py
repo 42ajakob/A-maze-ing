e413c1d4d0b0e00a660043f8e112e61331678626
anjakob <anjakob@student.42heilbronn.de>
2026-09-15 09:19:48 +0200

    removed validator.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,7 @@
-from .validator import validator
 from .parser import parser
 from .maze_gen import maze_gen
 from .out_file_creator import out_file_creator
 from .tui import tui
 """DocString"""
 
-__all__ = ["validator", "parser", "maze_gen", "out_file_creator", "tui"]
+__all__ = ["parser", "maze_gen", "out_file_creator", "tui"]
