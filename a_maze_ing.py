d112de41e2ce6813f20ac18eddb9ef1d5b833be5
anjakob <anjakob@student.42heilbronn.de>
2026-09-15 21:32:11 +0200

    moved try except block to a_maze_ing.py

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,14 +1,20 @@
+import sys
+
 from src.maze_builder import create_maze
 from src.tui import tui
 
 
 def main() -> int:
     """creates a maze and starts tui"""
-    config = create_maze()
-    tui(config.output_file, config)
+    try:
+        config = create_maze()
+    except Exception as e:
+        print(f"Error: {e}")
+        return 1
 
+    tui(config.output_file, config)
     return 0
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
--- src/maze_builder.py
+++ src/maze_builder.py
@@ -8,20 +8,16 @@ def create_maze() -> "MazeConfig":
     creates output file, returns config
     """
     config = parser()
-    try:
-        maze = MazeGenerator(
-            width=config.width,
-            height=config.height,
-            entry=config.maze_entry,
-            exit=config.maze_exit,
-            perfect=config.perfect,
-            seed=config.seed,
-        )
-        maze.generate()
-        write_maze(
-            config.output_file, maze
-        )
-    except Exception as e:
-        print(f"Error: {e}")
-        exit(1)
+    maze = MazeGenerator(
+        width=config.width,
+        height=config.height,
+        entry=config.maze_entry,
+        exit=config.maze_exit,
+        perfect=config.perfect,
+        seed=config.seed,
+    )
+    maze.generate()
+    write_maze(
+        config.output_file, maze
+    )
     return config
